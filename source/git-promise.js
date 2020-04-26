2459b7284f701c1d3c815460b4e1a875623bf43c
Wout Mertens <Wout.Mertens@gmail.com>
2020-04-26 13:36:14 +0200

    nodegit: handle stashing, lots of error wraps

--- source/git-promise.js
+++ source/git-promise.js
@@ -130,15 +130,15 @@ const gitExecutorProm = (args, retryCount) => {
  * Returns a promise that executes git command with given arguments.
  *
  * @function
- * @param {obj | Array} commands    - An object that represents all parameters or first parameter
- *                                  only, which is an array of commands.
- * @param {string}      repoPath    - path to the git repository.
- * @param {boolean=}    allowError  - true if return code of 1 is acceptable as some cases errors
- *                                  are acceptable.
- * @param {stream=}     outPipe     - if this argument exists, stdout is piped to this object.
- * @param {stream=}     inPipe      - if this argument exists, data is piped to stdin process on
- *                                  start.
- * @param {timeout=}    timeout     - execution timeout, default is 2 mins.
+ * @param {Object | string[]} commands    - An object that represents all parameters or first
+ *                                        parameter only, which is an array of commands.
+ * @param {string}            repoPath    - path to the git repository.
+ * @param {boolean=}          allowError  - true if return code of 1 is acceptable as some cases
+ *                                        errors are acceptable.
+ * @param {WritableStream=}   outPipe     - if this argument exists, stdout is piped to this object.
+ * @param {ReadableStream=}   inPipe      - if this argument exists, data is piped to stdin process
+ *                                        on start.
+ * @param {number=}           timeout     - execution timeout, default is 2 mins.
  * @returns {promise} Execution promise.
  * @example
  *
